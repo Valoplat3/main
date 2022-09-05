fcaf4d440855deb6415aeecfaf0927d6537e0e5f
Valoplat3 <111986651+Valoplat3@users.noreply.github.com>
2022-09-05 20:21:59 +0200

    Add files via upload

---
+++ NewClassHa.java
@@ -0,0 +1,11 @@
+public class NewClassHa   {
+
+    private String adress;
+    private double string;
+    private double length;
+
+    public NewClassHa(){
+        adress = "Kurfürstenstraße 22";
+        length = 20;
+    }
+}
